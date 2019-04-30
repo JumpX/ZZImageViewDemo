cc568ac853efe59dcd101c013c3846cf58243fdc
Jungle <xumianjun@yupaopao.cn>
2019-04-30 10:19:17 +0800

    Initial Commit

---
+++ ZZImageViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  
+//
+//  Created by Jungle on 2019/4/30.
+//  Copyright (c) 2019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZZImageViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZZImageViewDemo
+//
+//  Created by Jungle on 2019/4/30.
+//  Copyright (c) 2019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
